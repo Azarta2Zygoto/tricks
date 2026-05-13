2e2f016c8400f94e144278532f4152b62085081d
Azarta2Zygoto <quentin.potiron@gmail.com>
2026-05-13 23:54:48 +0200

    [ADD] Tourner une div

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Tricks
 
 Petit tricks simples faits purement en HTML-CSS-JS sans utilisation de librairies externes.
+
+## Listes des tricks
+
+- Faire tourner un élément non carré en le gardant centré.
---
+++ script.js
@@ -0,0 +1,85 @@
+function onChangeNumberColonnes(event) {
+  const flip = document.getElementById("flip");
+  // Supprimer tous les éléments enfants de la div "flip"
+  while (flip.firstChild) {
+    flip.removeChild(flip.firstChild);
+  }
+
+  // Récupérer le nombre de colonnes et de lignes à partir des champs d'entrée
+  const colonneNumber = parseInt(event.target.value);
+  const ligneNumber = parseInt(document.getElementById("ligne").value);
+
+  elementSize = getElementSize(colonneNumber, ligneNumber);
+
+  // Récupérer le flip des éléments
+  const currentValue = parseInt(flip.dataset.value);
+
+  // Générer les éléments en fonction du nombre de colonnes et de lignes
+  for (let i = 0; i < ligneNumber; i++) {
+    const elementRow = document.createElement("div");
+    elementRow.classList.add("element-row");
+    for (let j = 0; j < colonneNumber; j++) {
+      const element = document.createElement("div");
+      element.classList.add("element");
+      element.style.width = `${elementSize - 10}px`;
+      element.style.height = `${elementSize - 10}px`;
+      element.style.transform = `rotate(${-currentValue}deg)`;
+      element.textContent = i * colonneNumber + j + 1;
+      elementRow.appendChild(element);
+    }
+    flip.appendChild(elementRow);
+  }
+}
+
+function onChangeNumberLignes(event) {
+  const flip = document.getElementById("flip");
+  // Supprimer tous les éléments enfants de la div "flip"
+  while (flip.firstChild) {
+    flip.removeChild(flip.firstChild);
+  }
+
+  // Récupérer le nombre de colonnes et de lignes à partir des champs d'entrée
+  const colonneNumber = parseInt(document.getElementById("colonne").value);
+  const ligneNumber = parseInt(event.target.value);
+
+  elementSize = getElementSize(colonneNumber, ligneNumber);
+
+  // Récupérer le flip des éléments
+  const currentValue = parseInt(flip.dataset.value);
+
+  // Générer les éléments en fonction du nombre de colonnes et de lignes
+  for (let i = 0; i < ligneNumber; i++) {
+    const elementRow = document.createElement("div");
+    elementRow.classList.add("element-row");
+    for (let j = 0; j < colonneNumber; j++) {
+      const element = document.createElement("div");
+      element.classList.add("element");
+      element.style.width = `${elementSize - 10}px`;
+      element.style.height = `${elementSize - 10}px`;
+      element.style.transform = `rotate(${-currentValue}deg)`;
+      element.textContent = i * colonneNumber + j + 1;
+      elementRow.appendChild(element);
+    }
+    flip.appendChild(elementRow);
+  }
+}
+
+function getElementSize(colonneNumber, ligneNumber) {
+  const flip = document.getElementById("flip");
+  const flipWidth = flip.clientWidth;
+  const flipHeight = flip.clientHeight;
+  const minimum = Math.min(flipWidth, flipHeight);
+  return minimum / Math.max(colonneNumber, ligneNumber);
+}
+
+function onClickFlip(sign) {
+  const flip = document.getElementById("flip");
+  const currentValue = parseInt(flip.dataset.value);
+  const newValue = currentValue + 90 * sign;
+  flip.dataset.value = newValue;
+  flip.style.transform = `rotate(${newValue}deg)`;
+  const elements = flip.querySelectorAll(".element");
+  elements.forEach((element) => {
+    element.style.transform = `rotate(${-newValue}deg)`;
+  });
+}
